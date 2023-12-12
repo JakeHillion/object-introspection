72ffe19515b8911d2c78ca758d7bfc75ad46377c
Jake Hillion <jakehillion@meta.com>
2023-12-12 11:02:47 -0800

    incomplete: name type in compiler errors
    Summary:
    
    We have a good type representation in the Type Graph of an incomplete type and
    the underlying type that represents. However, this incomplete type still ends
    up in the generated code as `void` which loses information. For example, a
    container that can't contain void may fail to compile because it was
    initialised with `void` but really its because the type it was supposed to be
    initialised with (say, `Foo`) had incomplete debug information.
    
    This change identifies that a type is incomplete in the output by generating it
    as an incomplete type `struct Incomplete<struct Foo>`. This allows us to name
    the type correctly in the TreeBuilder output and filter for incomplete types,
    as well as getting appropriate compiler errors if it mustn't be incomplete.
    
    Test Plan:
    - CI
    - Added a unit test to namegen.
    - Enabled and added an extra pointers_incomplete test.
    
    This change is tricky to test because it isn't really user visible. The types
    still use their `inputName` which is unchanged in any successful output - this
    change is used so the compiler fails with a more detailed error.

--- test/test_enforce_compatibility.cpp
+++ test/test_enforce_compatibility.cpp
@@ -38,8 +38,8 @@ TEST(EnforceCompatibilityTest, VoidPointer) {
        R"(
 [0] Class: MyClass (size: 8)
       Member: p (offset: 0)
-[1]     Pointer
-          Incomplete
+[2]     Pointer
+[1]       Incomplete
             Primitive: void
 )",
        R"(
