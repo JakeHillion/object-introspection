b5756d2311d4c3b27c58366a6d711c32e33636bd
Jake Hillion <jakehillion@meta.com>
2023-12-22 17:14:58 +0000

    tbv2: use std::decay_t with smart pointers
    CodeGen v2 permits template parameters to be qualified. This means that if we
    call `make_field` with a template parameter it will be qualified. However, we
    don't qualify the types when generating meta functions such as `NameProvider`
    and `TypeHandler`. This means these qualified types don't match up with the
    expected type.
    
    Use `std::decay_t` when forwarding the type to `NameProvider` and `TypeHandler`
    so they're always the base type that they were generated with. Most of this is
    covered by `make_field`, but there are direct references to `TypeHandler<Ctx,
    T>` in a lot of `TypeHandler::type` fields. Fix the problematic types manually
    for now, there may need to be a better solution with meta functions for this in
    the future.
    
    Test Plan:
    - CI
    - Added a test for `std::unique_ptr<const uint64_t>` to exercise this. Failed
      before, passes after.
    - Added a test for `std::unique_ptr<const std::vector<uint64_t>>` to test a
      non-primitive type. Failed before, passes after.

--- oi/CodeGen.cpp
+++ oi/CodeGen.cpp
@@ -1072,9 +1072,9 @@ constexpr inst::Field make_field(std::string_view name) {
     sizeof(T),
     ExclusiveSizeProvider<T>::size,
     name,
-    NameProvider<T>::names,
-    TypeHandler<Ctx, T>::fields,
-    TypeHandler<Ctx, T>::processors,
+    NameProvider<std::decay_t<T>>::names,
+    TypeHandler<Ctx, std::decay_t<T>>::fields,
+    TypeHandler<Ctx, std::decay_t<T>>::processors,
   };
 }
 )";
